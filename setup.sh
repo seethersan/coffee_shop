54c86806e0c67c1d0cbc3d384da6d5388aaf71c3
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2022-11-16 20:41:25 -0500

    add env vars

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
-export EXCITED="true"
+export DATABASE_URL="postgres://nuyukgsdjomluk:bdbb2db3e9e25f44369f41b0de5c599d68b4bbc8106344991ef0d94a0f4fb8f7@ec2-3-227-68-43.compute-1.amazonaws.com:5432/dd934k39b1vj7u"
+export AUTH0_DOMAIN="dev-sp4x7fja44s1aayr.us.auth0.com"
+export API_AUDIENCE="http://localhost:8000/"
 echo "setup.sh script executed successfully!"
